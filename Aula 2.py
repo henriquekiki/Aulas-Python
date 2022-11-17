b365dfa89176efdc62c37f648e32428ee42c8ffa
henriquekiki <ricckdmo@gmail.com>
2022-11-17 17:10:37 -0300

    Primeiro repositorio sozinho

---
+++ Aula 2.py
@@ -0,0 +1,21 @@
+a = int(input('Primeiro valor:'))
+b = int(input('Segundo valor:'))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('Soma: {soma}.'
+      '\nSubtração: {sub}.'
+      '\nMultiplicação {multi}'
+      '\nDivisão: {div}'
+      '\nResto: {resto}'.format(soma=soma,
+                                sub=subtracao,
+                                multi=multiplicacao,
+                                div=divisao,
+                                resto=resto))
+# print(soma)
+# print(subtracao)
+# print(miltiplicacao)
+# print(divisao)
+# print(resto)
---
+++ Meu app.py
@@ -0,0 +1,5 @@
+print("meu primeiro programa em py")
+a = 2
+b = 3
+soma = a + b
+print(soma)
