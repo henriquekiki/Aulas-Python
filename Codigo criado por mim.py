ab245a3e9a07900add594b9eee1c238b19f382ed
henriquekiki <ricckdmo@gmail.com>
2022-11-18 21:55:40 -0300

    UP de mais um codigo

---
+++ Codigo criado por mim.py
@@ -0,0 +1,13 @@
+# Codigo criado por mim. Vou aperfeiçoando ele no decorrer do curso.
+# Pretendo fazer uma lista pra incluir nomes de animais.
+lista_animal = []
+a = input('Digite um animal: ')
+lista_animal.append(a)
+print('Animal adicionado.\n', lista_animal)
+while a:
+    a = input('Digite um animal: ')
+    lista_animal.append(a)
+    print('Animal adicionado.\n', lista_animal)
+
+
+
