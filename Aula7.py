008fcb13eede78ea5c679a70c4622f7f555a74e0
henriquekiki <ricckdmo@gmail.com>
2022-11-21 23:10:28 -0300

    Mais aulas concluidas.

---
+++ Aula7.py
@@ -0,0 +1,14 @@
+from Aula6_TV import Televisao
+from Aula6_2 import Calculadora
+from Aula8_importação import contador_letras
+
+
+if __name__ == '__main__':
+    televisao = Televisao()
+    print(televisao.ligada)
+    televisao.power()
+    print(televisao.ligada)
+    calculadora = Calculadora(10, 2)
+    print(calculadora.soma())
+    lista = ['cachorro', 'gato', 'elefante']
+    print(contador_letras(lista))
