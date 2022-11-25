0f0b778d83cd91d61d736c5ebea4d560ad8c384d
henriquekiki <ricckdmo@gmail.com>
2022-11-25 20:37:40 -0300

    Mais aulas concluidas

---
+++ Aula10.py
@@ -0,0 +1,41 @@
+from datetime import date, time, datetime, timedelta
+
+def trabalhando_com_datetime():
+    data_atual = datetime.now()
+    print(data_atual)
+    print(data_atual.strftime('%d/%m/%Y\n%H:%M:%S'))
+    print(data_atual.strftime('%c'))
+    print(data_atual.weekday())
+    tupla = ('Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabádo', 'Domingo')
+    print(tupla[data_atual.weekday()])
+    tupla_meses = ('0', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+    print(tupla_meses[data_atual.month])
+    data_criada = datetime(1995, 8, 30, 15, 30, 20)
+    print(data_criada)
+    print(data_criada.strftime('%c'))
+    data_string = '01/01/2022 12:20:22'
+    data_convertida = datetime.strptime(data_string, '%d/%m/%Y %H:%M:%S')
+    print(data_convertida)
+    nova_data = data_convertida - timedelta(days=365)
+    print(nova_data)
+
+
+
+def trabalhando_com_date():
+    data_atual = date.today()
+    data_atual_str = data_atual.strftime('%A %B %Y')
+    print(type(data_atual))
+    print(data_atual_str)
+    print(type(data_atual_str))
+
+def trabalhando_com_time():
+    horario = time(hour=15, minute=18, second=30)
+    print(horario)
+    horario_str = horario.strftime('%H:%M:%S')
+    print(horario_str)
+    print(type(horario_str))
+
+if __name__ == '__main__':
+    #trabalhando_com_date()
+    #trabalhando_com_time()
+    trabalhando_com_datetime()
